269994ef4db3708b6c4c660a5fa8e67c521689a3
Thomas Kroes <t.kroes@lumc.nl>
2021-10-10 11:03:25 +0200

    Fix dimension selection bug and present error when two dimensions are the same

--- src/MeanShiftClusteringPlugin.cpp
+++ src/MeanShiftClusteringPlugin.cpp
@@ -143,8 +143,10 @@ void MeanShiftClusteringPlugin::init()
     connect(&_settingsAction.getComputeAction(), &TriggerAction::triggered, this, [this, &inputDataset, &outputDataset, updateColors]() {
 
         // Do not run if the selected dimensions are the same
-        if (_settingsAction.getDimensionOneAction().getCurrentIndex() == _settingsAction.getDimensionTwoAction().getCurrentIndex())
+        if (_settingsAction.getDimensionOneAction().getCurrentIndex() == _settingsAction.getDimensionTwoAction().getCurrentIndex()) {
+            setTaskDescription("Input error: identical dimensions");
             return;
+        }
 
         // Update the sigma value
         _meanShift.setSigma(_settingsAction.getSigmaAction().getValue());
@@ -160,7 +162,7 @@ void MeanShiftClusteringPlugin::init()
         QCoreApplication::processEvents();
 
         std::vector<hdps::Vector2f> data;
-        inputDataset.extractDataForDimensions(data, 0, 1);
+        inputDataset.extractDataForDimensions(data, _settingsAction.getDimensionOneAction().getCurrentIndex(), _settingsAction.getDimensionTwoAction().getCurrentIndex());
 
         setTaskDescription("Clustering");
 
@@ -209,11 +211,11 @@ void MeanShiftClusteringPlugin::init()
         _settingsAction.setEnabled(true);
     });
 
-    connect(&_settingsAction.getDimensionOneAction(), &OptionAction::currentIndexChanged, this, [this](const std::int32_t& currentIndex) {
+    connect(&_settingsAction.getDimensionOneAction(), &OptionAction::currentIndexChanged, this, [this, dimensionNames](const std::int32_t& currentIndex) mutable -> void {
         _settingsAction.getComputeAction().trigger();
     });
     
-    connect(&_settingsAction.getDimensionTwoAction(), &OptionAction::currentIndexChanged, this, [this](const std::int32_t& currentIndex) {
+    connect(&_settingsAction.getDimensionTwoAction(), &OptionAction::currentIndexChanged, this, [this, dimensionNames](const std::int32_t& currentIndex) mutable -> void {
         _settingsAction.getComputeAction().trigger();
     });
 
