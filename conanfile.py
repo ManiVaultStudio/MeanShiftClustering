2cb9f6ed60c4e9aa43a0f125969d535830aa1309
bldrvnlw <b.van_lew@lumc.nl>
2022-04-05 18:07:41 +0200

    Inherit qt version from core
    Remove duplicate requirement to avoid version clashes on qt.

--- conanfile.py
+++ conanfile.py
@@ -31,8 +31,6 @@ class MeanShiftClusteringConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
 
-    requires = "qt/5.15.1@lkeb/stable"
-
     scm = {
         "type": "git",
         "subfolder": "hdps/MeanShiftClustering",
